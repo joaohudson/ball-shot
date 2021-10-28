660ff52f8be039f8e3a6e8795177a282e426e3c7
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-10-28 20:34:24 -0300

    atualizando dependencias e configurando porta

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-﻿const express = require('express');
+﻿require('dotenv/config');
+const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'client')));
