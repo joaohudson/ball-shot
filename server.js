2964973ee3de4a7227e845415df864aea9187de0
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-11-22 01:13:34 -0300

    adicionando pagina de 404

--- server.js
+++ server.js
@@ -2,5 +2,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'client', 'not-found.html'));
+});
+
 app.listen(process.env.PORT);
