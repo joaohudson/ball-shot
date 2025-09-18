fa205a4f9abd28c996c3df6465d5b260cc22b309
João Hudson <joaohudson29@gmail.com>
2025-09-18 11:28:53 -0300

    rename frontend folder

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'client', 'not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'docs', 'not-found.html'));
 });
 
 app.listen(process.env.PORT);
